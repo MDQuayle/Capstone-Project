4c3dda8bc6cccfbe7d193a489ca90f0e22493f0e
mdquayle <misternopants@gmail.com>
2022-03-05 08:56:40 -0500

    adds model relationships and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    belongs_to :user
+    has_many :replys
+    validates :title, presence: true
+    validates :content, presence: true
+
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,2 +1,6 @@
 class Reply < ApplicationRecord
+    belongs_to :post
+    belongs_to :user
+    validates :title, presence: true
+    validates :content, presence: true
 end
