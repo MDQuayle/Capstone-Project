bb8b97b8bf98bace25878a549bcd54b7e6e7276b
mdquayle <misternopants@gmail.com>
2022-03-05 09:25:45 -0500

    adds NBA route to posts controller, fixes indentation in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,37 @@
 class PostsController < ApplicationController
     
+	def nba
+		posts = Post.find_by(sport: "NBA")
+		render json: post
+	end
+	
+  def create
+    post = Post.create!(post_params)
+    render json: post , status: :created
+  end
     
-      def create
-        post = Post.create!(post_params)
-        render json: post , status: :created
-      end
+  def update
+  	post = Post.find_by(id: params[:id])
+    if post
+      post.update(post_params)
+      render json: post
+    else
+      render json: { error: "Post not found" }, status: :not_found
+    end
+  end
     
-      def update
-        post = Post.find_by(id: params[:id])
-        if post
-          post.update(post_params)
-          render json: post
-        else
-          render json: { error: "Post not found" }, status: :not_found
-        end
-      end
+  def destroy
+  	review = Post.find_by(id: params[:id])
+    if post
+      post.destroy
+      head :no_content
+    else
+      render json: { error: "Post not found" }, status: :not_found
+    end
+  end 
     
-      def destroy
-        review = Post.find_by(id: params[:id])
-        if post
-          post.destroy
-          head :no_content
-        else
-          render json: { error: "Post not found" }, status: :not_found
-        end
-      end 
-    
-      private
-      def post_params
-        params.permit(:sport, :title, :content,:user_id)
-      end
+  private
+  def post_params
+    params.permit(:sport, :title, :content,:user_id)
+  end
 end
