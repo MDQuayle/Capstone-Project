bef8102be66c616ee3fe9171bf3a6920b2bab6be
mdquayle <misternopants@gmail.com>
2022-03-22 17:54:52 -0400

    Adds code to display Posts in NBAPostsList and adds testing seed

--- client/src/components/NBA/NBAPostsList.js
+++ client/src/components/NBA/NBAPostsList.js
@@ -1,6 +1,10 @@
 import NBAPostsCard from 'NBAPostsCard'
-function NBAPostsList({posts, user}) {
-  const allPosts = posts.map((post) => <NBAPostsCard key = {post.id} id={post.id} post={post} user={user}/> )
+import { useSelector } from "react-redux";
+function NBAPostsList({user}) {
+  const posts =  useSelector((state) => state.posts)
+  const allPosts = posts.map((post) => (
+    <NBAPostsCard key={post.id} post={post} user={user} />
+  ))
   return(
   <div>
       {allPosts}
