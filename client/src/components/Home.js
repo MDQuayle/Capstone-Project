7e17e57dfaa1cbd5f8275a57781bd9ef74b9328f
mdquayle <misternopants@gmail.com>
2022-03-31 19:49:00 -0400

    adds user welcome to the home page and a check to see if the user is logged in

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,8 +3,18 @@ function Home() {
     return (
         <div>
             <h2>Thank you for choosing Sporum! Please log in to post!</h2>
-
-
+            <h1>
+        Welcome To Sporum!
+      </h1>
+      {user ? (
+        <div>
+          <p>Welcome {user.username}!</p>
+        </div>
+      ) : (
+          <div>
+            <p>Please login to start the conversation!</p>
+            </div>
+      )}
         </div>
     );
   }
